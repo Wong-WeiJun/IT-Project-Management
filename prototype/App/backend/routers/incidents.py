e359ab335538df41f5e25f08e7a606d3a90f47be
Wong Wei Jun <wong.weijun923@gmail.com>
2026-09-25 17:36:14 +1000

    feat: add get_all_incidents

--- prototype/App/backend/routers/incidents.py
+++ prototype/App/backend/routers/incidents.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
-from database import get_db
-from typing import Annotated
+from typing import Annotated, List
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from sqlalchemy.orm import Session
+
+from database import get_db
+import models
+import schemas
 
 router = APIRouter(prefix="/api/incidents", tags=["incidents"])
 
-@router.get("/")
-def get_all_incidents(db: Annotated[]):
-    
 
+@router.get("/", response_model=List[schemas.IncidentResponse])
+def get_all_incidents(db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Incident))
+    return result.scalars().all()
